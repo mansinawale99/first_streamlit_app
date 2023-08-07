11a7bae7fb13217625d1a2ca557e00ec78c778c2
Mansi Nawale <84671173+mansinawale99@users.noreply.github.com>
2023-08-07 11:45:02 -0400

    Update streamlit_app.py
    Added 4 line for text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My parents new helathy diner')
 
-streamlit.text('Breakfast Menu')
+streamlit.subheader('Breakfast Menu')
+streamlit.text('omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range egg')
