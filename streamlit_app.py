c4f0ada79b5ea2632c78684b7fe7f0b56f208d86
Mansi Nawale <84671173+mansinawale99@users.noreply.github.com>
2023-08-16 13:31:33 -0400

    Update streamlit_app.py
    Add Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
-#streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
