d6bc6fc1a935c8edf5b9d49aa49aaa3ad3f3ec8b
Mansi Nawale <84671173+mansinawale99@users.noreply.github.com>
2023-08-23 10:17:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,5 @@ import pandas
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+streamlit.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
